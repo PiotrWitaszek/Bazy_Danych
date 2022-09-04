3b8e92a2d6afe56913c7b9b53e04ca1960a705e2
Piotr Witaszek <p.a.witaszek@gmail.com>
2022-09-04 11:02:56 +0200

    adding update, select_where and delete_all

--- bazy2.py
+++ bazy2.py
@@ -1,6 +1,7 @@
 import sqlite3
 from sqlite3 import Error
 
+
 def create_connection(db_file):
     """create a database connection to the SQLite databes
     specified by db_file
@@ -9,12 +10,13 @@ def create_connection(db_file):
     """
     conn = None
     try:
-        conn = sqlite3.connect(db_file)  
+        conn = sqlite3.connect(db_file)
         return conn
     except Error as e:
         print(e)
 
     return conn
+
 
 def execute_sql(conn, sql):
     """Execute sql
@@ -28,20 +30,23 @@ def execute_sql(conn, sql):
     except Error as e:
         print(e)
 
-def add_project(conn, scorers):
+
+def add_scorers(conn, scorers):
     """
     Create a new scorers into highest_scorers table
     :param conn:
     :param scorers:
     return: project id
-    """      
+    """
     cur = conn.cursor()
-    cur.executemany('INSERT INTO highest_scorers VALUES(?, ?, ?)', scorers)
+    # używamy tutaj zmiennej globalnie zdeklarowane - to bym z chęcią omówił na spotkaniu jeszcze
+    cur.executemany("INSERT INTO highest_scorers VALUES(?, ?, ?)", scorers)
     conn.commit()
+
 
 def select_all():
     """
-    Query all rows in the table 
+    Query all rows in the table
     :param conn: the Conmnection object
     :return:
     """
@@ -51,11 +56,41 @@ def select_all():
 
     print(rows)
 
+
+def select_where(points):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM highest_scorers WHERE points=?", (points,))
+    rows = cur.fetchall()
+
+    print(rows)
+
+
+def update(points, id):
+    cur = conn.cursor()
+    cur.execute("UPDATE highest_scorers SET points=? WHERE id=?", (points, id))
+    conn.commit()
+
+
 def delete_where(id):
     cur = conn.cursor()
-    cur.execute('DELETE FROM highest_scorers WHERE id=?', (id,))
+    cur.execute("DELETE FROM highest_scorers WHERE id=?", (id,))
     conn.commit()
-  
+
+
+def delete_all():
+    """
+    Delete all rows from table
+    :param conn: Connection to the SQLite database
+    :param table: table name
+    :return:
+    """
+    sql = f"DELETE FROM highest_scorers"
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit
+    print("Deleted")
+
+
 if __name__ == "__main__":
     create_highest_scorers_sql = """
     -- highest_scorers table
@@ -71,20 +106,21 @@ if __name__ == "__main__":
     conn = create_connection(db_file)
     if conn is not None:
         execute_sql(conn, create_highest_scorers_sql)
-      
-    scorers = (
-        (None, 100, 'Wilt Chambarlain'),
-        (None, 81, 'Kobe Bryant'),
-        (None, 78, 'Wilt Chambarlain'),
-        (None, 73, 'David Thompson'),
-        (None, 73, 'Wilt Chambarlain'),
-        (None, 73, 'Wilt Chambarlain'),
-        (None, 72, 'Wilt Chambarlain'),
-        (None, 71, 'David Robinson'),
-        (None, 71, 'Elgin Baylor'), 
-    ) 
-
-    add_project(conn, scorers)
-    select_all()
-    delete_where(1)
-    select_all()
+        scorers = (
+            (None, 100, "Wilt Chambarlain"),
+            (None, 81, "Kobe Bryant"),
+            (None, 78, "Wilt Chambarlain"),
+            (None, 73, "David Thompson"),
+            (None, 73, "Wilt Chambarlain"),
+            (None, 73, "Wilt Chambarlain"),
+            (None, 72, "Wilt Chambarlain"),
+            (None, 71, "David Robinson"),
+            (None, 71, "Elgin Baylor"),
+        )
+        add_scorers(conn, scorers)
+        select_all()
+        delete_where(1)
+        update(85, 3)
+        select_where(73)
+        select_all()
+        delete_all()
